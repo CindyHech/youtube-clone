102658cd7cec0f1455563b6e8d906a00171335e5
Cindy Hech <archerychinadiva@gmail.com>
2026-07-23 02:57:10 -0400

    Sidebar section started

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 import "./Sidebar.css";
+import home from "../../assets-yt clone/home.png";
+import game_icon from "../../assets-yt clone/game_icon.png";
+import automobiles from "../../assets-yt clone/automobiles.png";
+import sports from "../../assets-yt clone/sports.png";
+import entertainment from "../../assets-yt clone/entertainment.png";
+import tech from "../../assets-yt clone/tech.png";
+import music from "../../assets-yt clone/music.png";
+import blogs from "../../assets-yt clone/blogs.png";
+import news from "../../assets-yt clone/news.png";
+import jack from "../../assets-yt clone/jack.png";
+import simon from "../../assets-yt clone/sports.png";
+import tom from "../../assets-yt clone/tom.png";
+import megan from "../../assets-yt clone/megan.png";
+import cameron from "../../assets-yt clone/cameron.png";
 
 const Sidebar = () => {
   return <div></div>;
