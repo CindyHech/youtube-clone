cde3bbce7d936e41ce3c3cde6c6672afd85b1a4b
Cindy Hech <archerychinadiva@gmail.com>
2026-08-11 22:35:10 -0400

    uncaught errors fixed

--- src/Components/Recommended/Recommended.jsx
+++ src/Components/Recommended/Recommended.jsx
@@ -1,20 +1,13 @@
 import React, { useEffect, useState } from "react";
 import "./Recommended.css";
-import thumbnail1 from "../../assets-yt clone/thumbnail1.png";
-import thumbnail2 from "../../assets-yt clone/thumbnail2.png";
-import thumbnail3 from "../../assets-yt clone/thumbnail3.png";
-import thumbnail4 from "../../assets-yt clone/thumbnail4.png";
-import thumbnail5 from "../../assets-yt clone/thumbnail5.png";
-import thumbnail6 from "../../assets-yt clone/thumbnail6.png";
-import thumbnail7 from "../../assets-yt clone/thumbnail7.png";
-import thumbnail8 from "../../assets-yt clone/thumbnail8.png";
-import { API_KEY } from "../../data";
+import { API_KEY, value_converter } from "../../data";
+import { Link } from "react-router-dom";
 
 const Recommended = ({ categoryId }) => {
   const [apiData, setApiData] = useState([]);
 
   const fetchData = async () => {
-    const relatedVideo__url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=45&regionCode=US&videoCategoryId=${categoryId}0&key=${API_KEY}`;
+    const relatedVideo__url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=45&regionCode=US&videoCategoryId=${categoryId}&key=${API_KEY}`;
     await fetch(relatedVideo__url)
       .then((res) => res.json())
       .then((data) => setApiData(data.items));
@@ -27,14 +20,18 @@ const Recommended = ({ categoryId }) => {
   return (
     <div className="recommended">
       {apiData.map((item, index) => (
-        <div key={index} className="side-video-list">
-          <img src={thumbnail1} alt="" />
+        <Link
+          to={`/video/${item.snippet.categoryId}/${item.id}`}
+          key={index}
+          className="side-video-list"
+        >
+          <img src={item.snippet.thumbnails.medium.url} alt="" />
           <div className="vid-info">
-            <h4>Best channel that help you to be a web developer</h4>
-            <p>GreatStack</p>
-            <p>199k Views</p>
+            <h4>{item.snippet.title}</h4>
+            <p>{item.snippet.channelTitle}</p>
+            <p>{value_converter(item.statistics.viewCount)} Views</p>
           </div>
-        </div>
+        </Link>
       ))}
     </div>
   );
